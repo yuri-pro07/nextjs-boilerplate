b3b86352db6e04c7f419b171ca2ce7e1bbb5def0
yuri-pro07 <yuriperuzzo9@gmail.com>
2025-04-21 22:23:03 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Nos editamos isso aqui{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
               app/page.tsx
             </code>
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Salvamos e updatamos no vercellllllll
           </li>
         </ol>
 
